9134e1862d7aff2b79def8212d359be6f41a2d2c
Evan Bay <neopythe@protonmail.ch>
2022-07-14 09:52:00 -0600

    improve styling

--- src/Joke.jsx
+++ src/Joke.jsx
@@ -1,18 +1,24 @@
 import React, { Component } from 'react'
 
+const emojis = ['😡', '😐', '😂']
+
 export default class Joke extends Component {
   render() {
     return (
-      <div className="flex justify-between w-full border">
-        <div className="flex flex-row">
+      <div className="flex justify-between items-center pl-4 pr-2 w-full border-b h-16">
+        <div className="flex flex-row items-center">
           <p>👍</p>
-          <p>{this.props.score}</p>
+          <div className="flex justify-center items-center rounded-full border-2 w-10 h-10 min-w-fit p-3 m-2 shadow-lg">
+            <p>{this.props.score}</p>
+          </div>
           <p>👎</p>
         </div>
         <p>{this.props.joke}</p>
-        {this.props.score > 0 && <p>😂</p>}
-        {this.props.score === 0 && <p>😐</p>}
-        {this.props.score < 0 && <p>😡</p>}
+        <p className="text-4xl">
+          {this.props.score > 0 && emojis[2]}
+          {this.props.score === 0 && emojis[1]}
+          {this.props.score < 0 && emojis[0]}
+        </p>
       </div>
     )
   }
--- src/JokeList.jsx
+++ src/JokeList.jsx
@@ -15,13 +15,17 @@ export default class JokeList extends Component {
   }
   render() {
     return (
-      <div className="flex justify-center items-center h-screen">
-        <header className="flex flex-col items-center justify-center gap-8 w-[300px] h-[600px] bg-green-500">
-          <h1 className="text-4xl font-bold text-white">Dad Jokes</h1>
-          <p>😂</p>
-          <button>new jokes</button>
+      <div className="flex justify-center items-center h-screen bg-slate-50">
+        <header className="flex flex-col items-center justify-center gap-8 w-[300px] h-[600px] bg-green-500 shadow-xl">
+          <h1 className="text-4xl font-bold text-white">
+            Dad <span className="font-thin">Jokes</span>
+          </h1>
+          <div className="flex justify-center items-center rounded-full shadow-2xl pb-6 pt-4 px-1 w-fit h-fit">
+            <p className="text-8xl">😂</p>
+          </div>
+          <button className="btn lowercase">new jokes</button>
         </header>
-        <ul className="flex flex-col justify-center items-center h-[550px] w-[700px] bg-slate-100">
+        <ul className="flex flex-col justify-center items-center h-[550px] w-[700px] bg-slate-100 shadow-xl">
           {this.state.jokes &&
             this.state.jokes.map((joke, index) => (
               <Joke
