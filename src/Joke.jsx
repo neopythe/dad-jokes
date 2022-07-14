eeb4293f22551126ec6bf2e9c299a80890e2a87e
Evan Bay <neopythe@protonmail.ch>
2022-07-14 09:24:24 -0600

    render Joke content dynamically through props

--- src/Joke.jsx
+++ src/Joke.jsx
@@ -3,14 +3,16 @@ import React, { Component } from 'react'
 export default class Joke extends Component {
   render() {
     return (
-      <div className="flex justify-between w-full">
+      <div className="flex justify-between w-full border">
         <div className="flex flex-row">
           <p>👍</p>
-          <p>⚪</p>
+          <p>{this.props.score}</p>
           <p>👎</p>
         </div>
-        <p>joke</p>
-        <p>😐</p>
+        <p>{this.props.joke}</p>
+        {this.props.score > 0 && <p>😂</p>}
+        {this.props.score === 0 && <p>😐</p>}
+        {this.props.score < 0 && <p>😡</p>}
       </div>
     )
   }
--- src/JokeList.jsx
+++ src/JokeList.jsx
@@ -3,6 +3,16 @@ import React, { Component } from 'react'
 import Joke from './Joke'
 
 export default class JokeList extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      jokes: [
+        { jokeText: 'funny', jokeScore: 5 },
+        { jokeText: 'meh', jokeScore: 0 },
+        { jokeText: 'awful', jokeScore: -1 },
+      ],
+    }
+  }
   render() {
     return (
       <div className="flex justify-center items-center h-screen">
@@ -12,10 +22,13 @@ export default class JokeList extends Component {
           <button>new jokes</button>
         </header>
         <ul className="flex flex-col justify-center items-center h-[550px] w-[700px] bg-slate-100">
-          <Joke />
-          <Joke />
-          <Joke />
-          <Joke />
+          {this.state.jokes &&
+            this.state.jokes.map((joke, index) => (
+              <Joke
+                joke={this.state.jokes[index].jokeText}
+                score={this.state.jokes[index].jokeScore}
+              />
+            ))}
         </ul>
       </div>
     )
