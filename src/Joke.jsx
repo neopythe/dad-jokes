50a780ba79022d7d987a8de6c3ef4b11213efd56
Evan Bay <neopythe@protonmail.ch>
2022-07-16 12:55:41 -0600

    replace reaction emoji with icon

--- src/Joke.jsx
+++ src/Joke.jsx
@@ -12,24 +12,22 @@ export default class Joke extends Component {
 
   render() {
     return (
-      <div className="flex justify-between items-center pl-6 pr-2 py-1 min-w-full border-b last:border-none">
+      <div className="flex justify-between items-center pl-6 pr-4 py-2 min-w-full gap-6 border-b last:border-none">
         <div className="flex flex-row items-center">
           <button onClick={this.props.upvote}>
             <BsHandThumbsUpFill className="text-[#ffd764] hover:text-[#e7bd3d]" />
           </button>
-          <div className="flex justify-center items-center rounded-full border-2 w-10 h-10 min-w-fit p-3 m-2 shadow-lg">
+          <div className="flex justify-center items-center rounded-full border-2 h-10 w-fit p-3 m-2 shadow-lg">
             <span>{this.props.score}</span>
           </div>
           <button onClick={this.props.downvote}>
             <BsHandThumbsDownFill className="text-[#ffd764] hover:text-[#e7bd3d]" />
           </button>
         </div>
-        <p className="mx-6 mr-auto pr-6 py-4">{this.props.joke}</p>
-        <p className="text-4xl">
-          {this.props.score > 0 && emojis[2]}
-          {this.props.score === 0 && emojis[1]}
-          {this.props.score < 0 && emojis[0]}
-        </p>
+        <p className="mr-auto py-4">{this.props.joke}</p>
+        <div>
+          <i className="em em-rolling_on_the_floor_laughing text-3xl shadow-lg rounded-full" />
+        </div>
       </div>
     )
   }
