830df0fc2561ef0279444bcdb5a86ed7c074c811
Evan Bay <neopythe@protonmail.ch>
2022-07-16 11:15:19 -0600

    replace upvote and downvote emojis with icons

--- src/Joke.jsx
+++ src/Joke.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 
+import { BsHandThumbsDownFill } from 'react-icons/bs'
+import { BsHandThumbsUpFill } from 'react-icons/bs'
+
 const emojis = ['😠', '😐', '🙂', '😀', '😆', '😂']
 
 export default class Joke extends Component {
@@ -11,14 +14,14 @@ export default class Joke extends Component {
     return (
       <div className="flex justify-between items-center pl-6 pr-2 py-1 min-w-full border-b last:border-none">
         <div className="flex flex-row items-center">
-          <button onClick={this.props.upvote} id="upvote">
-            👍
+          <button onClick={this.props.upvote}>
+            <BsHandThumbsUpFill className="text-[#ffd764] hover:text-[#e7bd3d]" />
           </button>
           <div className="flex justify-center items-center rounded-full border-2 w-10 h-10 min-w-fit p-3 m-2 shadow-lg">
             <span>{this.props.score}</span>
           </div>
-          <button onClick={this.props.downvote} id="downvote">
-            👎
+          <button onClick={this.props.downvote}>
+            <BsHandThumbsDownFill className="text-[#ffd764] hover:text-[#e7bd3d]" />
           </button>
         </div>
         <p className="mx-6 mr-auto pr-6 py-4">{this.props.joke}</p>
